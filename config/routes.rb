935c98473ac9e3376e603246631bc94867da7056
Sudheer <kumarsudheer4894@gmail.com>
2024-12-24 15:26:32 +0530

    CRUD Operation

--- config/routes.rb
+++ config/routes.rb
@@ -14,14 +14,15 @@ Rails.application.routes.draw do
 
   root "products#index"
 
+  # All these routes follow resources :products
   get "/products", to: "products#index"
 
-  get "/products/new", to: "products#new"
+  get "/products/new", to: "products#new", as: "new_product" 
   post "products", to: "products#create"
 
-  get "/products/:id", to: "products#show"
+  get "/products/:id", to: "products#show", as: "product"
 
-  get "/products/:id/edit", to: "products#edit"
+  get "/products/:id/edit", to: "products#edit", as: "edit_product"
   patch "/products/:id", to: "products#update"
   
   delete "/products/:id", to: "products#destroy"
